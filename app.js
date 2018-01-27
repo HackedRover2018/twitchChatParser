8182fe9a1b7d34ebc587d77a5d41f2c5fb35d317
brianofrim <bofrim@ualberta.ca>
2018-01-27 17:03:04 -0600

    echo set up

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var tmi = require('tmi.js');
+var http = require('http');
 var config = require('./config.json');
+
+var unreadMessages = [];
 
 var options = {
     options:{
@@ -25,4 +28,15 @@ client.on('connected', function(address,port){
 
 client.on("action", function (channel, userstate, message, self) {
     console.log(message);
+    unreadMessages.push(message);
 });
+
+
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write(JSON.stringify(ureadMessages));
+  unreadMessages = [];
+
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
